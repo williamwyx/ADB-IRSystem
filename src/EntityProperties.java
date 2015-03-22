3a4bd87bbb1c28bb70e8dc42d876ac19edf9c1fb
Yuxuan Wang <yw2666@columbia.edu>
2015-03-22 17:00:41 -0400

    Merge FBProperties.

--- src/EntityProperties.java
+++ src/EntityProperties.java
@@ -1,38 +1,76 @@
 /**
  * Created by qshen on 3/22/15.
  */
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class EntityProperties {
-    private static final HashMap<String, List<String>> types = new HashMap<String, List<String>>(){
-        {
-            put("Person", new ArrayList<String>(Arrays.asList("Name","Birthday","Place of Birth","Death","Siblings","Spouses","Description")));
-            put("Author", new ArrayList<String>(Arrays.asList("Books","Books About","Influenced","Influenced by")));
-            put("Actor", new ArrayList<String>(Arrays.asList("Films")));
-            put("Business Person", new ArrayList<String>(Arrays.asList("Leadership","Board Member","Founded")));
-            put("League", new ArrayList<String>(Arrays.asList("Name","Championship","Sport","Slogan","Official Website","Description","Teams")));
-            put("Sports Team", new ArrayList<String>(Arrays.asList("Name", "Description", "Sport","Arena","Championships","Coaches","Founded","Leagues","Locations","Players Roster")));
-        }
-    };
+	private static final HashMap<String, List<String>> types = new HashMap<String, List<String>>() {
+		{
+			put("Person",
+					new ArrayList<String>(Arrays.asList("Name", "Birthday",
+							"Place of Birth", "Date of Death", "Place of Death", "Cause of Death", "Siblings", "Spouses",
+							"Description")));
+			put("Author",
+					new ArrayList<String>(Arrays.asList("Books", "Books About",
+							"Influenced", "Influenced by")));
+			put("Actor", new ArrayList<String>(Arrays.asList("Films")));
+			put("Business Person",
+					new ArrayList<String>(Arrays.asList("Leadership",
+							"Board Member", "Founded")));
+			put("League",
+					new ArrayList<String>(Arrays.asList("Name", "Championship",
+							"Sport", "Slogan", "Official Website",
+							"Description", "Teams")));
+			put("Sports Team",
+					new ArrayList<String>(Arrays
+							.asList("Name", "Description", "Sport", "Arena",
+									"Championships", "Coaches", "Founded",
+									"Leagues", "Locations", "Players Roster")));
+		}
+	};
+	
+	private static final HashMap<String, String> subTypes = new HashMap<String, String>() {
+		{
+		}
+	};
 
-    private static final HashMap<String, String> FBTypes = new HashMap<String, String>(){
-        {
-            put("/people/person", "Person");
-            put("/book/author", "Author");
-            put("/film/actor", "Actor");
-            put("/tv/tv_actor", "Actor");
-            put("/organization/organization_founder", "Business Person");
-            put("/business/board_member", "Business Person");
-            put("/sports/sports_league", "League");
-            put("/sports/sports_team", "Sports Team");
-            put("/sports/professional_sports_team", "Sports Team");
-        }
-    };
+	private static final HashMap<String, String> FBTypes = new HashMap<String, String>() {
+		{
+			put("/people/person", "Person");
+			put("/book/author", "Author");
+			put("/film/actor", "Actor");
+			put("/tv/tv_actor", "Actor");
+			put("/organization/organization_founder", "Business Person");
+			put("/business/board_member", "Business Person");
+			put("/sports/sports_league", "League");
+			put("/sports/sports_team", "Sports Team");
+			put("/sports/professional_sports_team", "Sports Team");
+		}
+	};
 
-
-    private static final HashMap<String, String> temp = new HashMap<String, String>() {
-        {
-            //Business Person
+	private static final HashMap<String, String> FBProperties = new HashMap<String, String>() {
+		{
+			// Person
+			put("Person?Name", "/type/object/name");
+			put("Person?Birthday", "/people/person/date_of_birth");
+			put("Person?Place of Birth", "/people/person/place_of_birth");
+			put("Person?Date of Death", "/people/deceased_person/date_of_death");
+			put("Person?Place of Death", "/people/deceased_person/place_of_death");
+			put("Person?Cause of Death", "/people/deceased_person/cause_of_death");
+			put("Person?Siblings", "/people/person/sibling_s");
+			put("Person?Spouses", "/people/person/spouse_s");
+			put("Person?Description", "/common/topic/description");
+			// Author
+			put("Author?Books", "/book/author/works_written");
+			put("Author?Books About", "/book/book_subject/works");
+			put("Author?Influenced", "/influence/influence_node/influenced");
+			put("Author?Influenced", "/influence/influence_node/influenced_by");
+			// Actor
+			put("Actor?Films", "/film/actor/film");
+			//Business Person
             put("Business Person?Leadership", "/business/board_member/leader_of");
             put("Business Person?Board Member", "/business/board_member/organization_board_memberships");
             put("Business Person?Founded", "/organization/organization_founder/organizations_founded");
@@ -55,7 +93,6 @@ public class EntityProperties {
             put("Sports Team?Leagues", "/sports/sports_team/league");
             put("Sports Team?Locations", "/sports/sports_team/location");
             put("Sports Team?Players Roster", "/sports/sports_team/roster");
-        }
-    };
-
+		}
+	};
 }
--- src/IRSystem.java
+++ src/IRSystem.java
@@ -3,7 +3,7 @@ public class IRSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String query = "Bill Gates";
+		String query = "tolkien";
 		FreeBaseAPI fb = new FreeBaseAPI();
 		fb.infobox(query);
 	}
