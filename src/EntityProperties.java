95aa3b1b74f520213900e785abfb4978d5351b4b
RogerShen <qshen@dyn-160-39-254-212.dyn.columbia.edu>
2015-03-22 16:59:10 -0400

    add property mappings

--- src/EntityProperties.java
+++ src/EntityProperties.java
@@ -30,5 +30,32 @@ public class EntityProperties {
     };
 
 
+    private static final HashMap<String, String> temp = new HashMap<String, String>() {
+        {
+            //Business Person
+            put("Business Person?Leadership", "/business/board_member/leader_of");
+            put("Business Person?Board Member", "/business/board_member/organization_board_memberships");
+            put("Business Person?Founded", "/organization/organization_founder/organizations_founded");
+            //League
+            put("League?Name", "/type/object/name");
+            put("League?Championship", "/sports/sports_league/championship");
+            put("League?Sport", "/sports/sports_league/sport");
+            put("League?Slogan", "/organization/organization/slogan");
+            put("League?Official Website", "/common/topic/official_website");
+            put("League?Description", "/common/topic/description");
+            put("League?Teams", "/sports/sports_league/teams");
+            //Sports Team
+            put("Sports Team?Name", "/type/object/name");
+            put("Sports Team?Description", "/common/topic/description");
+            put("Sports Team?Sport", "/sports/sports_team/sport");
+            put("Sports Team?Arena", "/sports/sports_team/arena_stadium");
+            put("Sports Team?Championships", "/sports/sports_team/championships");
+            put("Sports Team?Coaches", "/sports/sports_team/coaches");
+            put("Sports Team?Founded", "/sports/sports_team/founded");
+            put("Sports Team?Leagues", "/sports/sports_team/league");
+            put("Sports Team?Locations", "/sports/sports_team/location");
+            put("Sports Team?Players Roster", "/sports/sports_team/roster");
+        }
+    };
 
 }
